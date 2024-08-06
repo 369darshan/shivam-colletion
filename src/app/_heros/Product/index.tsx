df90fff862319f4673624731d7b9216c709d8edb
Darshan Patel <40830928+369darshan@users.noreply.github.com>
2024-08-06 18:24:01 +0530

    update 0.4 cart update

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -24,50 +24,45 @@ export const ProductHero: React.FC<{
 
   return (
     <Gutter className={classes.productHero}>
-      <div className={classes.content}>
-        <div className={classes.categories}>
-          {categories?.map((category, index) => {
-            if (typeof category === 'object' && category !== null) {
-              const { title: categoryTitle } = category
+      <div className={classes.mediaWrapper}>
+        {!metaImage && <div className={classes.placeholder}>No image</div>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </div>
+      <div className={classes.details}>
+        <h3 className={classes.title}>{title}</h3>
+        <div className={classes.categoryWrapper}>
+          <div className={classes.categories}>
+            {categories?.map((category, index) => {
+              if (typeof category === 'object' && category !== null) {
+                const { title: categoryTitle } = category
 
-              const titleToUse = categoryTitle || 'Untitled category'
+                const titleToUse = categoryTitle || 'Untitled category'
 
-              const isLast = index === categories.length - 1
+                const isLast = index === categories.length - 1
 
-              return (
-                <Fragment key={index}>
-                  {titleToUse}
-                  {!isLast && <Fragment>, &nbsp;</Fragment>}
-                </Fragment>
-              )
-            }
+                return (
+                  <p key={index} className={classes.category}>
+                    {titleToUse}
+                    {!isLast && <Fragment>, &nbsp;</Fragment>}
+                    <span className={classes.separator}>|</span>
+                  </p>
+                )
+              }
 
-            return null
-          })}
-        </div>
-        <h1 className={classes.title}>{title}</h1>
-        <div>
-          <p className={classes.description}>
-            {`${description ? `${description} ` : ''}To edit this product, `}
-            <Link href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}>
-              navigate to the admin dashboard
-            </Link>
-            {'.'}
-          </p>
+              return null
+            })}
+          </div>
+          <p className={classes.stock}>In stock</p>
         </div>
         <Price product={product} button={false} />
-        <AddToCartButton product={product} className={classes.addToCartButton} />
-      </div>
-      <div className={classes.media}>
-        <div className={classes.mediaWrapper}>
-          {!metaImage && <div className={classes.placeholder}>No image</div>}
-          {metaImage && typeof metaImage !== 'string' && (
-            <Media imgClassName={classes.image} resource={metaImage} fill />
-          )}
+
+        <div className={classes.description}>
+          <h6>Description</h6>
+          <p>{description}</p>
         </div>
-        {metaImage && typeof metaImage !== 'string' && metaImage?.caption && (
-          <RichText content={metaImage.caption} className={classes.caption} />
-        )}
+        <AddToCartButton product={product} className={classes.addToCartButton} />
       </div>
     </Gutter>
   )
